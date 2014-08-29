99d5dd519c82ec5d2f72e3e0bfc1063c00219db9
Nicholas Wiersma <nick@wiersma.co.za>
2014-08-29 11:03:17 +0200

    Initial commit

---
+++ mod_php56_apache2/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Amazon Web Services"
+license          "Apache 2.0"
+description      "Installs/Configures Apache with mod_php5"
+version          "0.1"
+
+supports "ubuntu"
+
+depends "apache2"
+depends "deploy"
---
+++ mod_php56_apache2/recipes/default.rb
@@ -0,0 +1,25 @@
+include_recipe 'apache2'
+
+# add repository for PHP 5.4
+apt_repository "php54" do
+  uri "http://ppa.launchpad.net/ondrej/php5-5.6/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
+end
+ 
+# update apt info
+execute "apt-get update" do
+  command "apt-get update"
+  ignore_failure true
+end
+ 
+# update PHP to version 5.6 and add necessary packages
+%w{php5-common php5-intl php-apc php5-mysqlnd php5-curl}.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+include_recipe 'apache2::mod_php5'
