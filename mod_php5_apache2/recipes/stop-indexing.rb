69d1949022765b7ab1325e047ef2b92c066c654f
Nicholas Wiersma <nick@wiersma.co.za>
2015-01-06 11:21:00 +0200

    Reload apache when we are done

--- mod_php5_apache2/recipes/stop-indexing.rb
+++ mod_php5_apache2/recipes/stop-indexing.rb
@@ -1,3 +1,5 @@
+include_recipe 'apache2'
+
 template "#{node['apache']['dir']}/robots.txt" do
   source 'robots.txt.erb'
   owner 'root'
@@ -19,4 +21,8 @@ link "#{node['apache']['conf_enabled_dir']}/staging.conf" do
   to "#{node['apache']['conf_available_dir']}/staging.conf"
   owner 'root'
   group 'root'
+end
+
+service "apache2" do
+  action :reload
 end
